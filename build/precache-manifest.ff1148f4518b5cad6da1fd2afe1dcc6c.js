5e8fe37f5644baa8ee95b2a44445236b4cedf79f
Abdurahmon703 <79171311+Abdurahmon703@users.noreply.github.com>
2021-04-03 23:04:29 +0500

    Add files via upload

---
+++ build/precache-manifest.ff1148f4518b5cad6da1fd2afe1dcc6c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6536926bd2c24400eabfe7e81cf360e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbfe0736f9253700a6f4",
+    "url": "/static/css/main.7ff83acc.chunk.css"
+  },
+  {
+    "revision": "362a5120baa3b6674112",
+    "url": "/static/js/2.3bef0eac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3bef0eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfe0736f9253700a6f4",
+    "url": "/static/js/main.390e1a56.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "a0461ecb9c103b18572a788edfc70f08",
+    "url": "/static/media/1.a0461ecb.jpg"
+  },
+  {
+    "revision": "3f92a7f19af68d68de19515007b0d157",
+    "url": "/static/media/2.3f92a7f1.jpg"
+  },
+  {
+    "revision": "ffe6b6b3a757d27df8975eeb40f70727",
+    "url": "/static/media/card-image.ffe6b6b3.jpg"
+  },
+  {
+    "revision": "ee7ab3ca9f77589a9a2e65e9bc57a4d7",
+    "url": "/static/media/pexels-pixabay-270348.ee7ab3ca.jpg"
+  },
+  {
+    "revision": "ad3d667eaed06c86f1cd72a99f089117",
+    "url": "/static/media/samarkand.ad3d667e.jpg"
+  }
+]);
